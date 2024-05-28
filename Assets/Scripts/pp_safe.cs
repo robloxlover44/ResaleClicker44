d872ac417a2221aaf57f71c73bebd00c8b66f04e
robloxlover44 <danza.davydov@gmail.com>
2024-05-28 18:11:51 +0300

    locks on second shop slide

---
+++ Assets/Scripts/pp_safe.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+public class pp_safe : MonoBehaviour
+{
+  
+}
